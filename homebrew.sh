8a77ff8f07c72839bd9ff26aac9b7019d0ea2c1f
Mike Moate <github@mikemoate.co.uk>
2020-05-05 00:05:26 +0100

    Fine tuned homebrew scripting

--- homebrew.sh
+++ homebrew.sh
@@ -1,4 +1,9 @@
 #! /bin/sh
 
-# brew install $(cat formulae.txt)
+brew install $(cat formulae.txt)
+
+brew tap AdoptOpenJDK/openjdk
 brew cask install $(cat casks.txt)
+
+npm install -g yarn # can't install yarn from homebrew without pulling the latest, non-LTS, node
+npm install -g expo-cli # no homebrew package for expo-cli
